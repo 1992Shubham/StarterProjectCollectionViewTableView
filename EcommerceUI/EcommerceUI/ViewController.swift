943f90202ae1aa621e8b6d8a69c94e322b97891d
Shubham Agarwal <34255689+1992Shubham@users.noreply.github.com>
2021-02-19 20:16:57 +0530

    Update ViewController.swift
    Load Json method added.

--- EcommerceUI/EcommerceUI/ViewController.swift
+++ EcommerceUI/EcommerceUI/ViewController.swift
@@ -11,7 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadJson()
         // Do any additional setup after loading the view.
+    }
+    
+    func loadJson() {
+        if let path = Bundle.main.path(forResource: "EcommerceJson", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                let jsonData = try JSONSerialization.data(withJSONObject: jsonResult, options: .prettyPrinted)
+                let jsonDecoder = JSONDecoder()
+                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+                productData = try jsonDecoder.decode(ProductModel.self, from: jsonData)
+                
+              } catch {
+                   // handle error
+              }
+        }
     }
 
 
